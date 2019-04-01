ebe6ffdbb207da39c383fa3e7f68cff86228bc44
Dustin Jamner <dijamner@me.com>
2019-03-31 20:01:01 -0400

    Simplify keyboard setup, remap shift

--- input.sh
+++ input.sh
@@ -2,9 +2,6 @@
 
 # TODO: configure key bindings
 
-#exit after an error
-set -e
-
 # Script location
 my_dir="$(dirname "$(realpath -s $0)")"
 
@@ -23,6 +20,7 @@ password_loc="~/Passwords.kdbx"
 xkb="/usr/share/X11/xkb"
 ctrl_file="symbols/ctrl"
 us_file="symbols/us"
+custom_file="symbols/custom"
 evdev="rules/evdev"
 evdevxml="rules/evdev.xml"
 
@@ -42,53 +40,32 @@ kappendconfig5 () {
 # Setup Steps #
 ###############
 
-list-rswap-evdev () {
-    grep -q swap_ralt_rctl "$xkb/$evdev"
-    if [ $? -eq 1 ]
-    then
-        echo "Listing swap_ralt_rctl in $xkb/$evdev..."
-        sudo sed -i "/^\s*ctrl:swap_lalt_lctl\s*=/r $my_dir/xkb/$evdev" "$xkb/$evdev"
-    else
-        echo "swap_ralt_rctl found in $xkb/$evdev, skipping...."
-    fi
-}
-
-# TODO: necessary? if so, fix
-list-rswap-evdev-xml () {
-    grep -q swap_ralt_rctl "$xkb/$evdevxml"
-    if [ $? -eq 1 ]
-    then
-        echo "Listing swap_ralt_rctl in $xkb/$evdevxml..."
-        sed "/^$/r $my_dir/xkb/$evdevxml" "$xkb/$evdevxml"
-    else
-        echo "swap_ralt_rctl found in $xkb/$evdevxml, skipping...."
-    fi
-}
-
 keyboard () {
-    #TODO set up keybindings, ability to switch to greek easily
-    # https://manpages.debian.org/testing/keyboard-configuration/keyboard.5.en.html
-    #add-greek
-    #set-caps-lock-switch
-    #swap-left-ctrl-alt
+    # TODO: make update automatically (rght now, does not update if present)
     
     # TODO: write data to basic/evdev files to register layout variant
     
-    list-rswap-evdev
+    #list-rswap-evdev
     #list-rswap-evdev-xml
+    #append-if-not-present "$my_dir/xkb/$ctrl_file" "$xkb/$ctrl_file"
+    #append-if-not-present "$my_dir/xkb/$us_file" "$xkb/$us_file"
     
-    append-if-not-present "$my_dir/xkb/$ctrl_file" "$xkb/$ctrl_file"
-    append-if-not-present "$my_dir/xkb/$us_file" "$xkb/$us_file"
-    # sets two layouts: us(dustin) and greek
+    # move custom layout file into place
+    sudo cp "$my_dir/xkb/$custom_file" "$xkb/$custom_file"
+    
+    # register options in evdev
+    append-if-not-present "$my_dir/xkb/$evdev" "$xkb/$evdev"
+    
+    # sets two layouts: us and greek
     # resets all options
     # sets caps lock to switch between layouts
     # swaps ctrl and alt for both left and right
     # TODO: set permanently instead of just for session
-    setxkbmap -layout us,gr -variant dustin, -option -option "ctrl:swap_ralt_rctl,ctrl:swap_lalt_lctl,grp:caps_toggle"
-    # TODO: Ctrl-tab for switching programs (swap with alt-tab?)
-    #TODO: Ctrl ~ for switching widnows
-    #TODO: consider using caps-lock for one of above
+    setxkbmap -layout us,gr -option -option "custom:swap_ralt_rctl_rshift,custom:numpad_arrow_tri,ctrl:swap_lalt_lctl,grp:caps_toggle"
     #keybindings made of alternate layout + alternate shortcuts (maybe?)
+    # TODO: change multimedia keys to default vs fn keys (in bios?)
+    # TODO: map right shift to alt, right ctrl to alt_gr
+    # TODO: map numlk 1 + numlk3 to ctrl+shift+tab, ctrl+tab
 }
 
 
